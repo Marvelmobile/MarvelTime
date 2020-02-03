34c52b752bec921667141caf4fe0ca617528d585
Marvel Mobile <fulanodetal@exemplo.br>
2020-02-03 19:54:54 -0300

    Projeto integrador - Teste

--- app/src/main/java/br/digitalhouse/marveltime/MainActivity.java
+++ app/src/main/java/br/digitalhouse/marveltime/MainActivity.java
@@ -3,6 +3,7 @@ package br.digitalhouse.marveltime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Teste", "Teste");
     }
 }
