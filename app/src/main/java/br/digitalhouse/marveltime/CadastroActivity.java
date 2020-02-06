b2e82ee3891452d61bfebb9c06927b81d99ec2cb
adelaniasantos <adelania.psantos@gmail.com>
2020-02-06 21:37:28 -0200

    Adelania - 06/02: Layout Completo

--- app/src/main/java/br/digitalhouse/marveltime/CadastroActivity.java
+++ app/src/main/java/br/digitalhouse/marveltime/CadastroActivity.java
@@ -2,13 +2,27 @@ package br.digitalhouse.marveltime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private TextInputLayout cadastrarNome;
+    private TextInputLayout cadastrarEmail;
+    private TextInputLayout cadastrarSenha;
+    private TextInputLayout cadastrarSenhaConfirmacao;
+    private FloatingActionButton bntCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
     }
+
 }
