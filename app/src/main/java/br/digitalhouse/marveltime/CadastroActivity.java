de44b4528862f861df0937b7482e159e5a333a7d
adelaniasantos <adelania.psantos@gmail.com>
2020-02-06 21:45:29 -0200

    Adelania/Inghridy - 06/02: Validação tela cadastro

--- app/src/main/java/br/digitalhouse/marveltime/CadastroActivity.java
+++ app/src/main/java/br/digitalhouse/marveltime/CadastroActivity.java
@@ -12,6 +12,7 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class CadastroActivity extends AppCompatActivity {
 
+
     private TextInputLayout cadastrarNome;
     private TextInputLayout cadastrarEmail;
     private TextInputLayout cadastrarSenha;
@@ -23,6 +24,148 @@ public class CadastroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
+        initViews();
+
+        bntCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomeCompleto = cadastrarNome.getEditText().getText().toString();
+                String email = cadastrarEmail.getEditText().getText().toString();
+                String senha = cadastrarSenha.getEditText().getText().toString();
+                String senhaConfirmacao = cadastrarSenhaConfirmacao.getEditText().getText().toString();
+
+                if (ehVazio(nomeCompleto) || ehVazio(email) || ehVazio(senha) || ehVazio(senhaConfirmacao))
+                    notificacao();
+                else if (!nomeValido(nomeCompleto))
+                    notificacaoNomeCompleto();
+                else if (!emailValido(email))
+                    notificacaoEmailInvalido();
+                else if (senhaValida(senha)) {
+                    if (!senhaIguais(senha, senhaConfirmacao))
+                        notificacaoSenhaDiferente();
+                    else
+                        notificacaoParaProximaTela();
+                } else
+                    notificacaoSenhaInvalida();
+            }
+        });
     }
 
+
+
+    private void initViews() {
+        cadastrarNome = findViewById(R.id.cadastrarNome);
+        cadastrarEmail = findViewById(R.id.cadastrarEmail);
+        cadastrarSenha = findViewById(R.id.cadastrarSenha);
+        cadastrarSenhaConfirmacao = findViewById(R.id.cadastrarSenhaConfirmacao);
+        bntCadastrar = findViewById(R.id.bntCadastrar);
+    }
+
+    //INGHRIDY SANTOS  - 05/02/20 - BDEV02 - VALIDAÇÃO USUARIO
+    private boolean nomeValido(String nomeCompleto) {
+
+        if (nomeCompleto.contains(" "))
+            return true;
+        else
+            return false;
+    }
+
+    private boolean emailValido(String email) {
+        if (email.contains("@") && email.contains(".com"))
+            return true;
+        else
+            return false;
+    }
+    //INGHRIDY SANTOS  - 05/02/20 - BDEV03 - VALIDAÇÃO SENHA
+    private boolean senhaValida(String senha) {
+        if (senha.length() < 6)
+            return false;
+
+        boolean achouNumero = false;
+        boolean achouMaiuscula = false;
+        boolean achouMinuscula = false;
+        boolean achouSimbolo = false;
+
+        for (char c : senha.toCharArray()) {
+            if (c >= '0' && c <= '9')
+                achouNumero = true;
+            else if (c >= 'A' && c <= 'Z')
+                achouMaiuscula = true;
+            else if (c >= 'a' && c <= 'z')
+                achouMinuscula = true;
+            else
+                achouSimbolo = true;
+        }
+        return achouNumero && achouMaiuscula && achouMinuscula && achouSimbolo;
+    }
+
+    private boolean senhaIguais(String senha, String confirmacaoSenha) {
+        if (confirmacaoSenha.equals(senha))
+            return true;
+        else
+            return false;
+    }
+
+    private boolean ehVazio(String valorComparacao) {
+        if (valorComparacao.isEmpty())
+            return true;
+        else
+            return false;
+    }
+
+
+    //INGHRIDY SANTOS  - 05/02/20 - BDEV02 - NOTIFICAÇÕES
+    protected void notificacao() {
+        Context contexto = getApplicationContext();
+        String textoNotificacao = "Por favor, preencha todos os campos";
+        int duracaoNotifacao = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(contexto, textoNotificacao, duracaoNotifacao);
+        toast.show();
+    }
+
+    protected void notificacaoNomeCompleto() {
+        Context contexto = getApplicationContext();
+        String textoNotificacao = "Por favor, preencha o nome completo para cadastro no Marvel Time";
+        int duracaoNotifacao = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(contexto, textoNotificacao, duracaoNotifacao);
+        toast.show();
+    }
+
+    protected void notificacaoSenhaInvalida() {
+        Context contexto = getApplicationContext();
+        String textoNotificacao = "A senha deve conter pelo menos 6 caracteres, uma letra maiuscula e minuscula, um número e um caracter especial.";
+        int duracaoNotifacao = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(contexto, textoNotificacao, duracaoNotifacao);
+        toast.show();
+    }
+
+    protected void notificacaoSenhaDiferente() {
+        Context contexto = getApplicationContext();
+        String textoNotificacao = "Os dados inseridos nos campos Senha e Confirme sua senha, devem ser iguais. Tente novamente.";
+        int duracaoNotifacao = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(contexto, textoNotificacao, duracaoNotifacao);
+        toast.show();
+    }
+
+    protected void notificacaoParaProximaTela() {
+        Context contexto = getApplicationContext();
+        String textoNotificacao = "PARABÉNS, em instantes você será direcionado a próxima tela.";
+        int duracaoNotifacao = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(contexto, textoNotificacao, duracaoNotifacao);
+        toast.show();
+    }
+
+    protected void notificacaoEmailInvalido() {
+        Context contexto = getApplicationContext();
+        String textoNotificacao = "Por favor, insira um e-mail válido";
+        int duracaoNotifacao = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(contexto, textoNotificacao, duracaoNotifacao);
+        toast.show();
+    }
 }
