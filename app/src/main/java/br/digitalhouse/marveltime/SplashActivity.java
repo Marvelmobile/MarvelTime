a4146659bf019c25d660f66aa857db98d11c0ff6
Marvelmobile <60369256+Marvelmobile@users.noreply.github.com>
2020-02-08 17:22:50 -0300

    Atualização splash

--- app/src/main/java/br/digitalhouse/marveltime/SplashActivity.java
+++ app/src/main/java/br/digitalhouse/marveltime/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash2);
 
         timer.schedule(new TimerTask() {
             @Override
