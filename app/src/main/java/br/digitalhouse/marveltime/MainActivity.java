4c132291f27180eb042e2e051e781790b8036ba8
Marvelmobile <60369256+Marvelmobile@users.noreply.github.com>
2020-02-17 17:32:22 -0300

    Desenvolvimento inicial perguntas do Quiz e implementação de fragments

--- app/src/main/java/br/digitalhouse/marveltime/MainActivity.java
+++ app/src/main/java/br/digitalhouse/marveltime/MainActivity.java
@@ -2,20 +2,33 @@ package br.digitalhouse.marveltime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button homemAranha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //INGHRIDY - 03/02/20 - SET01- Criação das Branchs GITHUB
-        Log.i("Teste", "Teste");
-        Log.i("Teste", "Teste - branch epic");
-        Log.i("Teste", "Teste - branch epic");
-        Log.i("Teste", "Teste - branch epic");
+        homemAranha = findViewById(R.id.homemAranha);
+
+        homemAranha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                direcionaPerguntas();
+            }
+        });
+
+    }
+
+    private void direcionaPerguntas(){
+        startActivity(new Intent(MainActivity.this, RecebePerguntasQuizActivity.class));
     }
 }
