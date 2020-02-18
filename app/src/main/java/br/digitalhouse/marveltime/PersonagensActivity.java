d2a1230994c860b02270829324256b5717040c74
Marvelmobile <60369256+Marvelmobile@users.noreply.github.com>
2020-02-18 01:38:42 +0000

    Tela Personagens/Quiz

---
+++ app/src/main/java/br/digitalhouse/marveltime/PersonagensActivity.java
@@ -0,0 +1,14 @@
+package br.digitalhouse.marveltime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonagensActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personagens);
+    }
+}
---
+++ app/src/main/java/br/digitalhouse/marveltime/QuizActivity.java
@@ -0,0 +1,16 @@
+package br.digitalhouse.marveltime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
