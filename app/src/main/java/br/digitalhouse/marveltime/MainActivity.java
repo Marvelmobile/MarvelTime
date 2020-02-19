6b4f0442804ea1555daaac1f919c9f7358704384
Marvelmobile <marvelmobiledh@gmail.com>
2020-02-18 23:06:18 -0300

    Adelania - Links tela Quiz e personagens

--- app/src/main/java/br/digitalhouse/marveltime/MainActivity.java
+++ app/src/main/java/br/digitalhouse/marveltime/MainActivity.java
@@ -3,6 +3,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.michaldrabik.tapbarmenulib.TapBarMenu;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,11 +16,15 @@ public class MainActivity extends AppCompatActivity {
     private CircleImageView imageViewPersonagens;
     private CircleImageView imageViewFavoritos;
     private CircleImageView imageViewQuiz;
-  
+
+    @BindView(R.id.tapBarMenu)
+    TapBarMenu tapBarMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         initViews();
         imageViewQuiz.setOnClickListener(new View.OnClickListener() {
@@ -43,5 +53,33 @@ public class MainActivity extends AppCompatActivity {
         imageViewFavoritos = findViewById(R.id.img_favoritos);
         imageViewPersonagens = findViewById(R.id.img_personagens);
         imageViewQuiz = findViewById(R.id.img_quiz);
+    }
+
+    @OnClick(R.id.tapBarMenu)
+    public void onMenuButtonClick() {
+        tapBarMenu.toggle();
+    }
+
+    @OnClick({ R.id.item1, R.id.item2, R.id.item3, R.id.item4 })
+    public void onMenuItemClick(View view) {
+        tapBarMenu.close();
+        switch (view.getId()) {
+            case R.id.item1:
+                //Home
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                break;
+            case R.id.item2:
+                //Favoritos
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                break;
+            case R.id.item3:
+                //Personagens
+                startActivity(new Intent(MainActivity.this, PersonagensActivity.class));
+                break;
+            case R.id.item4:
+                //Quiz
+                startActivity(new Intent(MainActivity.this, QuizActivity.class));
+                break;
+        }
     }
 }
