50817a6c349669563954a64f650af767a2c551fc
backhenry <henrique03martins@gmail.com>
2020-03-24 12:26:55 -0300

    Sprint 4

---
+++ app/src/main/java/br/digitalhouse/marveltime/Fragments/ResultadoFragment.java
@@ -0,0 +1,18 @@
+package br.digitalhouse.marveltime.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.digitalhouse.marveltime.R;
+
+public class ResultadoFragment extends Fragment {
+
+
+}
---
+++ app/src/main/java/br/digitalhouse/marveltime/Models/Questao.java
@@ -0,0 +1,43 @@
+package br.digitalhouse.marveltime.Models;
+
+public class Questao {
+    private String nome, pergunta, alternativa1, alternativa2, alternativa3, alternativa4, resposta;
+
+    public Questao(String nome, String pergunta, String alternativa1, String alternativa2, String alternativa3, String alternativa4, String resposta) {
+        this.nome = nome;
+        this.pergunta = pergunta;
+        this.alternativa1 = alternativa1;
+        this.alternativa2 = alternativa2;
+        this.alternativa3 = alternativa3;
+        this.alternativa4 = alternativa4;
+        this.resposta = resposta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public String getAlternativa1() {
+        return alternativa1;
+    }
+
+    public String getAlternativa2() {
+        return alternativa2;
+    }
+
+    public String getAlternativa3() {
+        return alternativa3;
+    }
+
+    public String getAlternativa4() {
+        return alternativa4;
+    }
+
+    public String getResposta() {
+        return resposta;
+    }
+}
