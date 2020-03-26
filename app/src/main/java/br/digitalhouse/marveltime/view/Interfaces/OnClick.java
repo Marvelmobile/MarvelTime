88ca5b1bad23e8b03a936dc7c7c541026464ad93
adelaniasantos <adelania.psantos@gmail.com>
2020-03-25 22:09:33 -0300

    Adelania - Estruturação MVVM

---
+++ app/src/main/java/br/digitalhouse/marveltime/view/Interfaces/OnClick.java
@@ -0,0 +1,7 @@
+package br.digitalhouse.marveltime.view.Interfaces;
+import br.digitalhouse.marveltime.model.PersonagemResult;
+
+public interface OnClick {
+
+    void click(PersonagemResult personagem);
+}
