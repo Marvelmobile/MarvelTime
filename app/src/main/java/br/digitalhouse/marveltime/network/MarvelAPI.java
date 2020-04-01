52f8b7f720c742bc03edce2e3ff10c73196a9e5c
Ingsantos <khllnghrd@gmail.com>
2020-04-01 18:42:15 -0300

    01/04/2020 - starting api implementation

--- app/src/main/java/br/digitalhouse/marveltime/network/MarvelAPI.java
+++ app/src/main/java/br/digitalhouse/marveltime/network/MarvelAPI.java
@@ -1,5 +1,19 @@
 package br.digitalhouse.marveltime.network;
 
+import br.digitalhouse.marveltime.model.PersonagemResponse;
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface MarvelAPI {
 
+    @GET( "comics?" )
+    Single<PersonagemResponse> getPersonagens(
+            @Query( "format" ) String format,
+            @Query ( "formatType" ) String formatType,
+            @Query ( "noVariants" ) boolean noVariants,
+            @Query ( "orderBy" ) String orderBy,
+            @Query ( "ts" ) String ts,
+            @Query ( "hash" ) String hash,
+            @Query ( "apikey" ) String apikey);
 }
