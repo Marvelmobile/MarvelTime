cadf2e39b90628b5ad5fee4bcf484c9e1fff053a
backhenry <henrique03martins@gmail.com>
2020-04-04 17:05:18 -0300

    Sprint 5 Quiz Melhorias

---
+++ app/src/main/java/br/digitalhouse/marveltime/Interfaces/HelperQuiz.java
@@ -0,0 +1,8 @@
+package br.digitalhouse.marveltime.Interfaces;
+
+public interface HelperQuiz {
+    void correto(int correto);
+    void errado(int errado);
+    void troca();
+    void titulo(String titulo);
+}
