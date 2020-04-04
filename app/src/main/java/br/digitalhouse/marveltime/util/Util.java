152bd6e358a0bcdb79819ce21d339ea4d90dfd05
Cristiane <54155908+crissbr@users.noreply.github.com>
2020-04-04 15:51:21 -0300

    Update Util.java
    Linhas removidas.

--- app/src/main/java/br/digitalhouse/marveltime/util/Util.java
+++ app/src/main/java/br/digitalhouse/marveltime/util/Util.java
@@ -1,9 +1,7 @@
 package br.digitalhouse.marveltime.util;
-
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -12,7 +10,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Util {
-
     public static boolean verificaConexaoComInternet(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo;
