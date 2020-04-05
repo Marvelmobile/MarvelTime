bb80a521f1523e4a2478c36e2d6786ef775ac4a6
adelaniasantos <adelania.psantos@gmail.com>
2020-04-05 14:31:48 -0300

    Ade - Nova pre_epic

---
+++ app/src/main/java/br/digitalhouse/marveltime/view/Interfaces/OnClick.java
@@ -0,0 +1,6 @@
+package br.digitalhouse.marveltime.view.Interfaces;
+import br.digitalhouse.marveltime.model.PersonagemResult;
+
+public interface OnClick {
+        void click(PersonagemResult personagem);
+ }
