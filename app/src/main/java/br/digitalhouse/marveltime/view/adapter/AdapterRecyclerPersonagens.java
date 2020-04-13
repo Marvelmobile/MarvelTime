a16f3393360e5175c8ce37d76dc8a1a8d03a29a3
adelaniasantos <adelania.psantos@gmail.com>
2020-04-13 00:10:19 -0300

    Adelania - Sprint 6 Salvar dados API

--- app/src/main/java/br/digitalhouse/marveltime/view/adapter/AdapterRecyclerPersonagens.java
+++ app/src/main/java/br/digitalhouse/marveltime/view/adapter/AdapterRecyclerPersonagens.java
@@ -42,10 +42,10 @@ public class AdapterRecyclerPersonagens extends RecyclerView.Adapter<ViewHolderP
     }
 
     public void atualizaLista(List<PersonagemResult> novaLista) {
-        if (this.personagemResultsList.isEmpty()) {
-            this.personagemResultsList = novaLista;
-        } else {
-            this.personagemResultsList.addAll(novaLista);
+        for (PersonagemResult personagemResult : novaLista) {
+            if(!personagemResult.getThumbnail().getPath().contains("image_not_available")){
+                this.personagemResultsList.add(personagemResult);
+            }
         }
         notifyDataSetChanged();
     }
