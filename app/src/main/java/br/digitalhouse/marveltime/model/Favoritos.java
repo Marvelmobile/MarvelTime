9ff01f5f14c94e9b4416f4579dfd8165396faa54
Henrique Baptista Martins <backhenry@users.noreply.github.com>
2020-04-16 02:05:29 -0300

    Henrique Sprint 6

---
+++ app/src/main/java/br/digitalhouse/marveltime/model/Favoritos.java
@@ -0,0 +1,30 @@
+package br.digitalhouse.marveltime.model;
+
+public class Favoritos {
+    private PersonagemResult personagemResult;
+    private CardModel cardModelquestão;
+
+    public Favoritos(PersonagemResult personagemResult) {
+        this.personagemResult = personagemResult;
+    }
+
+    public Favoritos(CardModel cardModelquestão) {
+        this.cardModelquestão = cardModelquestão;
+    }
+
+    public PersonagemResult getPersonagemResult() {
+        return personagemResult;
+    }
+
+    public void setPersonagemResult(PersonagemResult personagemResult) {
+        this.personagemResult = personagemResult;
+    }
+
+    public CardModel getCardModelquestão() {
+        return cardModelquestão;
+    }
+
+    public void setCardModelquestão(CardModel cardModelquestão) {
+        this.cardModelquestão = cardModelquestão;
+    }
+}
