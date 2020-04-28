1647f28433ee7a7c812ec6c0ca85a63d6574faa7
Henrique Baptista Martins <backhenry@users.noreply.github.com>
2020-04-28 19:51:32 -0300

    Henrique - Login Google

--- app/src/main/java/br/digitalhouse/marveltime/view/activity/CadastroActivity.java
+++ app/src/main/java/br/digitalhouse/marveltime/view/activity/CadastroActivity.java
@@ -1,54 +1,89 @@
 package br.digitalhouse.marveltime.view.activity;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 import br.digitalhouse.marveltime.R;
 import br.digitalhouse.marveltime.util.Helper;
+
 import static br.digitalhouse.marveltime.util.Constantes.CHAVE_EMAIL;
 
 public class CadastroActivity extends AppCompatActivity {
+    static final int GOOGLE_SIGN = 123;
     private TextInputLayout cadastrarNome;
     private TextInputLayout cadastrarEmail;
     private TextInputLayout cadastrarSenha;
     private TextInputLayout cadastrarSenhaConfirmacao;
     private Button bntCadastrar;
+    private FloatingActionButton btngoogle;
     private FirebaseAuth mFirebaseAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
         initViews();
+        google();
         mFirebaseAuth = FirebaseAuth.getInstance();
         bntCadastrar.setOnClickListener(v -> cadastraNovoUsuario());
     }
 
+    private void google() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        btngoogle.setOnClickListener(v -> SignInGoogle());
+    }
+
+    void SignInGoogle() {
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent, GOOGLE_SIGN);
+    }
+
     private void cadastraNovoUsuario() {
 
-        if(validaCampos(Helper.getString(cadastrarNome), Helper.getString(cadastrarEmail),
-                Helper.getString(cadastrarSenha), Helper.getString(cadastrarSenhaConfirmacao))){
+        if (validaCampos(Helper.getString(cadastrarNome), Helper.getString(cadastrarEmail),
+                Helper.getString(cadastrarSenha), Helper.getString(cadastrarSenhaConfirmacao))) {
             mFirebaseAuth.createUserWithEmailAndPassword(Helper.getString(cadastrarEmail),
                     Helper.getString(cadastrarSenhaConfirmacao))
                     .addOnCompleteListener(CadastroActivity.this, task -> {
-                                if (!task.isSuccessful()){
-                                    Toast.makeText(CadastroActivity.this,
-                                            task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                                } else {
-                                    voltarTelaLoginActivity(Helper.getString(cadastrarEmail));
-                                }
-                            });
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(CadastroActivity.this,
+                                    task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        } else {
+                            voltarTelaLoginActivity(Helper.getString(cadastrarEmail));
+                        }
+                    });
         }
     }
 
     private void voltarTelaLoginActivity(String email) {
-        if(!email.isEmpty()) {
+        if (!email.isEmpty()) {
             Intent returnIntent = new Intent();
-            returnIntent.putExtra(CHAVE_EMAIL,email);
+            returnIntent.putExtra(CHAVE_EMAIL, email);
             setResult(RESULT_OK, returnIntent);
         }
         finish();
@@ -60,6 +95,7 @@ public class CadastroActivity extends AppCompatActivity {
         cadastrarSenha = findViewById(R.id.cadastrarSenha);
         cadastrarSenhaConfirmacao = findViewById(R.id.cadastrarSenhaConfirmacao);
         bntCadastrar = findViewById(R.id.bntCadastrar);
+        btngoogle = findViewById(R.id.loginGoogle);
     }
 
     private void notificacao(String sMensagem) {
@@ -84,5 +120,32 @@ public class CadastroActivity extends AppCompatActivity {
         } else
             notificacao(getString(R.string.regra_senha));
         return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GOOGLE_SIGN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn
+                    .getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        firebaseUser = mFirebaseAuth.getCurrentUser();
+                    } else {
+                        notificacao("Falha ao tentar logar com o Google");
+                    }
+                });
     }
 }
--- app/src/main/java/br/digitalhouse/marveltime/view/activity/LoginActivity.java
+++ app/src/main/java/br/digitalhouse/marveltime/view/activity/LoginActivity.java
@@ -1,5 +1,8 @@
 package br.digitalhouse.marveltime.view.activity;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,22 +13,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 import org.jetbrains.annotations.NotNull;
+
 import br.digitalhouse.marveltime.R;
 import br.digitalhouse.marveltime.util.Helper;
+
+import static android.widget.Toast.LENGTH_LONG;
 import static br.digitalhouse.marveltime.util.Constantes.Activity_UM_DOIS;
 import static br.digitalhouse.marveltime.util.Constantes.CHAVE_EMAIL;
 
 public class LoginActivity extends AppCompatActivity {
+    static final int GOOGLE_SIGN = 123;
     private TextInputLayout loginUsuario;
     private TextInputLayout loginSenha;
     private Button bntLogin;
+    private FloatingActionButton btngoogle;
     private TextView loginRegistro;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,33 +54,78 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         initViews();
         linkCadastroUsuario();
+        google();
         firebaseAuth = FirebaseAuth.getInstance();
-
         authStateListener = firebaseAuth -> {
-            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-            if (firebaseUser != null){
+            firebaseUser = firebaseAuth.getCurrentUser();
+            if (firebaseUser != null) {
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         };
-
         bntLogin.setOnClickListener(v -> {
-            if(validaCampos()){
+            if (validaCampos()) {
                 firebaseAuth.signInWithEmailAndPassword(Helper.getString(loginUsuario),
                         Helper.getString(loginSenha)).addOnCompleteListener(LoginActivity.this,
                         task -> {
-                    if (!task.isSuccessful()){
-                        Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                    } else {
-                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                    }
-                });
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, task.getException().getMessage(), LENGTH_LONG).show();
+                            } else {
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            }
+                        });
             }
         });
-
-        loginRegistro.setOnClickListener(v -> startActivityForResult(new Intent(LoginActivity.this, CadastroActivity.class),1));
+        loginRegistro.setOnClickListener(v -> startActivityForResult(new Intent(LoginActivity.this, CadastroActivity.class), 1));
     }
 
-    private boolean validaCampos(){
+    private void google() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        btngoogle.setOnClickListener(v -> SignInGoogle());
+    }
+
+    void SignInGoogle() {
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent, GOOGLE_SIGN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Activity_UM_DOIS) {
+            if (resultCode == RESULT_OK) {
+                loginUsuario.getEditText().setText(data.getStringExtra(CHAVE_EMAIL));
+            }
+        }
+        if (requestCode == GOOGLE_SIGN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn
+                    .getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        firebaseUser = firebaseAuth.getCurrentUser();
+                    } else {
+                        notificacao("Falha ao tentar logar com o Google");
+                    }
+                });
+    }
+
+
+    private boolean validaCampos() {
         if (Helper.isEmptyString(Helper.getString(loginUsuario)) || Helper.isEmptyString(Helper.getString(loginSenha)))
             notificacao(getString(R.string.preencher_campos));
         else if (!Helper.usuarioValido(Helper.getString(loginUsuario)) || !Helper.senhaValida(Helper.getString(loginSenha)))
@@ -70,15 +136,6 @@ public class LoginActivity extends AppCompatActivity {
         return false;
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == Activity_UM_DOIS) {
-            if (resultCode == RESULT_OK) {
-                loginUsuario.getEditText().setText(data.getStringExtra(CHAVE_EMAIL));
-            }
-        }
-    }
-
     private void linkCadastroUsuario() {
         String msgRegistroUsuario = getString(R.string.registre_se);
         int i1 = msgRegistroUsuario.indexOf("[");
@@ -86,25 +143,27 @@ public class LoginActivity extends AppCompatActivity {
 
         loginRegistro.setMovementMethod(LinkMovementMethod.getInstance());
         loginRegistro.setText(msgRegistroUsuario, TextView.BufferType.SPANNABLE);
-        Spannable mySpannable = (Spannable)loginRegistro.getText();
+        Spannable mySpannable = (Spannable) loginRegistro.getText();
 
         ClickableSpan myClickableSpan = new ClickableSpan() {
             @Override
-            public void onClick(@NotNull View widget) { }
+            public void onClick(@NotNull View widget) {
+            }
         };
         mySpannable.setSpan(myClickableSpan, i1, i2 + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
     }
 
-    private void initViews (){
+    private void initViews() {
         loginUsuario = findViewById(R.id.loginUsuario);
         loginSenha = findViewById(R.id.loginSenha);
         bntLogin = findViewById(R.id.btnLogin);
         loginRegistro = findViewById(R.id.loginRegistro);
+        btngoogle = findViewById(R.id.loginGoogle);
     }
 
-    private void notificacao (String sMensagem){
+    private void notificacao(String sMensagem) {
         Context contexto = getApplicationContext();
-        Toast toast = Toast.makeText(contexto, sMensagem, Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(contexto, sMensagem, LENGTH_LONG);
         toast.show();
     }
 
